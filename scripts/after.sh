bff851ca6be1bbd26258b6b1d868d1417327b53c
imba久期 <imba97@qq.com>
2023-05-19 12:45:52 +0800

    after.sh 优化

--- scripts/after.sh
+++ scripts/after.sh
@@ -4,22 +4,18 @@
 
 LOCK_FILE=after.lock
 
-if [ -f ${LOCK_FILE} ]; then
-    # Lock file already exists, exit the script
-    echo "An instance of this script is already running"
-    exit 1
+if [ ! -f ${LOCK_FILE} ]; then
+    # source ${HOME}/.zshrc
+
+    # 安装 nodejs
+    nvm install 16.14.0
+    nvm use 16.14.0
+
+    # 设置镜像源
+    npm config set registry https://registry.npm.taobao.org
+
+    # 安装 npm 包
+    npm i -g yarn pnpm @vue/cli
+
+    echo "Locked" > ${LOCK_FILE}
 fi
-
-source ${HOME}/.zshrc
-
-# 安装 nodejs
-nvm install 16.14.0
-nvm use 16.14.0
-
-# 设置镜像源
-npm config set registry https://registry.npm.taobao.org
-
-# 安装 npm 包
-npm i -g yarn pnpm @vue/cli
-
-echo "Locked" > ${LOCK_FILE}
