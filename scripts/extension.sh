f2b1fa81810393d4de2a18bf85e23b977fbcf4a4
imba97 <mail@imba97.cn>
2024-11-18 15:11:53 +0800

    feat: update extension

--- scripts/extension.sh
+++ scripts/extension.sh
@@ -3,18 +3,19 @@
 # 安装 vscode 插件
 
 exts=(
-  # 中文，各种方式配置语言包均不生效
-  # MS-CEINTL.vscode-language-pack-zh-hans
-  # Volar
+  MS-CEINTL.vscode-language-pack-zh-hans
   Vue.volar
-  # 颜色选择器
   anseki.vscode-color
-  # typescript
   ms-vscode.vscode-typescript-next
-  # Prettier
-  esbenp.prettier-vscode
-  # GitLens
   eamodio.gitlens
+  YoavBls.pretty-ts-errors
+  foxundermoon.shell-format
+  dbaeumer.vscode-eslint
+  usernamehw.errorlens
+  antfu.unocss
+  antfu.iconify
+  antfu.icons-carbon
+  antfu.theme-vitesse
 )
 
 for ext in ${exts[@]}
