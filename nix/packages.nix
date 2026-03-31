d35d964e468285ef9ba1e079dcd9548c82826139
imba97 <mail@imba97.cn>
2026-04-01 02:07:06 +0800

    feat: improve frontend dev env in nix

--- nix/packages.nix
+++ nix/packages.nix
@@ -2,10 +2,23 @@
 { pkgs }:
 with pkgs;
 [
-  nodejs
+  nodejs_22
+  corepack
   yarn
   pnpm
   nodePackages.nrm
+  nodePackages.npm-check-updates
+  nodePackages.eslint
+  python3
+  gcc
+  gnumake
+  pkg-config
+  jq
+  ripgrep
+  fd
+  fzf
+  git-lfs
+  gh
   git
   vim
 ]
